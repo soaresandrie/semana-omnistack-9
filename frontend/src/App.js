bacf49dfa4c130322106c42603c64cda324b357e
soaresandrie <jsoaresandrie@gmail.com>
2019-10-22 23:57:56 -0300

    login and cross-route navigation functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
+
 import './App.css';
 
-import logo from "./assets/logo.svg"
+import logo from "./assets/logo.svg";
+
+import Routes from "./routes";
 
 function App() {
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
+      
       <div className="content" >
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail"></input>
-          <button className="btn" type="submit">Entrar</button>
-        </form>
-
+        <Routes />
       </div>
-
     </div>
   );
 }
